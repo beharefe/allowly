4d33f640f1df9f0a38fbd1487fd7f9eed26c4efe
Efe <efe@efebehar.dev>
2026-03-04 18:07:27 +0100

    feat(wallet): add acp

--- packages/app/components/WalletProvider.tsx
+++ packages/app/components/WalletProvider.tsx
@@ -10,6 +10,7 @@ import {
   PhantomWalletAdapter,
   SolflareWalletAdapter,
 } from "@solana/wallet-adapter-wallets";
+import { ActionCodesWalletAdapter } from "@actioncodes/wallet-adapter";
 import config from "@/constants";
 
 export function SolanaWalletProvider({
@@ -20,8 +21,16 @@ export function SolanaWalletProvider({
   const endpoint = config.rpcUrl;
 
   const wallets = useMemo(
-    () => [new PhantomWalletAdapter(), new SolflareWalletAdapter()],
-    [],
+    () => [
+      new PhantomWalletAdapter(),
+      new ActionCodesWalletAdapter({
+        authToken: process.env.NEXT_PUBLIC_ACTIONCODES_AUTH_TOKEN || "",
+        connection: endpoint,
+        environment: "mainnet",
+      }),
+      new SolflareWalletAdapter(),
+    ],
+    [endpoint],
   );
 
   return (
